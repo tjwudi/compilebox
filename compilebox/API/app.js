8c4e78574c3c52fcac19a0e9c9d52c106e96ef02
tjwudi <webmaster@leapoahead.com>
2016-02-19 21:44:49 +0800

    Reformat
    Signed-off-by: tjwudi <webmaster@leapoahead.com>

--- compilebox/API/app.js
+++ compilebox/API/app.js
@@ -52,7 +52,18 @@ app.post('/compile',bruteforce.prevent,function(req, res)
     var timeout_value=20;//Timeout Value, In Seconds
 
     //details of this are present in DockerSandbox.js
-    var sandboxType = new sandBox(timeout_value,path,folder,vm_name,arr.compilerArray[language][0],arr.compilerArray[language][1],code,arr.compilerArray[language][2],arr.compilerArray[language][3],arr.compilerArray[language][4],stdin);
+    var sandboxType = new sandBox(
+        timeout_value,
+        path,
+        folder,
+        vm_name,
+        arr.compilerArray[language][0],
+        arr.compilerArray[language][1],
+        code,
+        arr.compilerArray[language][2],
+        arr.compilerArray[language][3],
+        arr.compilerArray[language][4],
+        stdin);
 
 
     //data will contain the output of the compiled/interpreted code
